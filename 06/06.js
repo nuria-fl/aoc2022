5b87a5268ccffbad3075f6dc0d969aa77452d077
Nuria <soriano.nuria@gmail.com>
2022-12-15 12:59:08 +0100

    day 6 part 2

--- 06/06.js
+++ 06/06.js
@@ -14,7 +14,21 @@ function solveStep1(input) {
   }
 }
 
-function solveStep2(input) {}
+function solveStep2(input) {
+  const data = input.split("");
+  for (let i = 13; i < data.length; i++) {
+    let last14Characters = data.slice(i - 13, i + 1);
+    if (
+      !last14Characters.some((c, idx) => {
+        const arr = [...last14Characters];
+        arr.splice(idx, 1);
+        return arr.includes(c);
+      })
+    ) {
+      return i + 1;
+    }
+  }
+}
 
 module.exports = {
   solveStep1,
--- 06/06.test.js
+++ 06/06.test.js
@@ -2,33 +2,55 @@ const { solveStep1, solveStep2 } = require("./06");
 describe("first step", () => {
   it("example 1 should be 7", () => {
     const result = solveStep1("mjqjpqmgbljsphdztnvjfqwrcgsmlb");
-  
+
     expect(result).toBe(7);
-  })
+  });
   it("example 2 should be 5", () => {
     const result = solveStep1("bvwbjplbgvbhsrlpgdmjqwftvncz");
-  
+
     expect(result).toBe(5);
-  })
+  });
   it("example 3 should be 6", () => {
     const result = solveStep1("nppdvjthqldpwncqszvftbrmjlhg");
-  
+
     expect(result).toBe(6);
-  })
+  });
   it("example 4 should be 10", () => {
     const result = solveStep1("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg");
-  
+
     expect(result).toBe(10);
-  })
+  });
   it("example 5 should be 11", () => {
     const result = solveStep1("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw");
-  
+
     expect(result).toBe(11);
-  })
+  });
 });
 
-xit("second step should return MCD", () => {
-  const result = solveStep2("/05/05.values-example.txt");
+describe("second step", () => {
+  it("example 1 should be 19", () => {
+    const result = solveStep2("mjqjpqmgbljsphdztnvjfqwrcgsmlb");
 
-  expect(result).toBe("MCD");
+    expect(result).toBe(19);
+  });
+  it("example 2 should be 23", () => {
+    const result = solveStep2("bvwbjplbgvbhsrlpgdmjqwftvncz");
+
+    expect(result).toBe(23);
+  });
+  it("example 3 should be 23", () => {
+    const result = solveStep2("nppdvjthqldpwncqszvftbrmjlhg");
+
+    expect(result).toBe(23);
+  });
+  it("example 4 should be 29", () => {
+    const result = solveStep2("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg");
+
+    expect(result).toBe(29);
+  });
+  it("example 5 should be 26", () => {
+    const result = solveStep2("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw");
+
+    expect(result).toBe(26);
+  });
 });
